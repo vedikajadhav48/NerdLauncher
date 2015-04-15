933ce9c13fe5f23c8d67ea19de81b51a248a6b4f
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-04-15 12:54:43 -0700

    NerdLauncher complete Chp 23

---
+++ app/src/main/java/com/example/vedikajadhav/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,16 @@
+package com.example.vedikajadhav.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new NerdLauncherFragment();
+    }
+}
